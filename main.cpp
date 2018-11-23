9fe156efd9891db5623e19bd76d189517656a786
Ahmad Alobaid <ahmad88me@gmail.com>
2018-11-23 15:12:06 +0100

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,9 @@ using namespace std;
 int main(){
 	EasyLogger logger("test.log");
 	logger.log("testing");
+	EasyLogger *logger_ptr;
+	logger_ptr = new EasyLogger("test.log");
+	logger_ptr->log("ptr testing");
 	cout << "Easy Logger\n";
 	return 0;
 }
